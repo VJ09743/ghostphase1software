ab0861536b10beba4b2c64478720348b34baf6ce
VJ09743 <viraj.verma0616@gmail.com>
2026-05-26 20:37:15 +0200

    First2done

---
+++ Variables/Variables.java
@@ -0,0 +1,9 @@
+package Variables;
+
+public class Variables {
+    public static void main(){
+        String RobotName  = "Blinky";
+        Integer MaxSpeed = 180;
+        Integer w = 7 , x = 3 , y = 3 , z = 3;
+    }
+}
